d00383b4d4ded58aa813bc498171995564a320ef
vijayan007 <vijayan.srinivasan@gmail.com>
2017-02-04 11:59:48 +0530

    First Commit

---
+++ 1-hello-world/src/HelloWorld.java
@@ -0,0 +1,30 @@
+/**
+ *  There are 3 types of comments<br/>
+ *  1. <b>//</b>  - Single-Line Comment<br/>
+ *  2. <b>/*</b>  - Paragraph Comment<br/>
+ *  3. <b>/**</b> - Java Doc Comment<br/>
+ * 
+ * 
+ * @author Vijayan Srinivasan
+ * @since Feb 4, 2017 11:43:45 AM
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * This is the starting place for execution
+	 */
+	public static void main(String[] args) {
+		//This program is coded to print just "Hello World"
+		
+		/*
+		 * This is a sample paragraph comment
+		 * 
+		 * Adding one more line 
+		 *  
+		 */
+		
+		System.out.println("Hello World");
+	}
+
+}
