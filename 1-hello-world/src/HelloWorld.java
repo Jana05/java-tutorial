cc337c0b11f96f0453bf5ca8026ba1ec7a5b5c7e
vsrinivasan <vsrinivasan@BANLWE754A1427.corp.intuit.net>
2017-02-04 12:12:46 +0530

    added link for keywords

--- 1-hello-world/src/HelloWorld.java
+++ 1-hello-world/src/HelloWorld.java
@@ -1,9 +1,10 @@
 /**
- *  There are 3 types of comments<br/>
- *  1. <b>//</b>  - Single-Line Comment<br/>
- *  2. <b>/*</b>  - Paragraph Comment<br/>
- *  3. <b>/**</b> - Java Doc Comment<br/>
+ * There are 3 types of comments<br/>
+ * 1. <b>//</b> - Single-Line Comment<br/>
+ * 2. <b>/*</b> - Paragraph Comment<br/>
+ * 3. <b>/**</b> - Java Doc Comment<br/>
  * 
+ * <p>List of keywords can be found <a href="http://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html">here</a></p>
  * 
  * @author Vijayan Srinivasan
  * @since Feb 4, 2017 11:43:45 AM
@@ -15,15 +16,15 @@ public class HelloWorld {
 	 * This is the starting place for execution
 	 */
 	public static void main(String[] args) {
-		//This program is coded to print just "Hello World"
-		
+		// This program is coded to print just "Hello World"
+
 		/*
 		 * This is a sample paragraph comment
 		 * 
-		 * Adding one more line 
-		 *  
+		 * Adding one more line
+		 * 
 		 */
-		
+
 		System.out.println("Hello World");
 	}
 
