d99e80df7a046527ab48e025b8e8ed5623d00f4d
Jana05 <janarthanan.boopalan@gmail.com>
2017-02-27 22:09:23 +0530

    Maven Projects added (#28)
    * First Maven Project
    
    * First Maven Project
    
    * Learn Maven
    
    * deleted wrong project
    
    * maven web app

---
+++ 1-chapter/11-maven-hello-world/src/main/java/pkg1/HelloWold.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package pkg1;
+
+/**
+ * @author Vijayan Srinivasan
+ * @author Janarthanan Shanmugam   
+ * @since Feb 26, 2017 9:19:01 PM
+ * 
+ */
+public class HelloWold {
+
+	public static void main(String[] args) {
+		System.out.println("maven hello world!!!");
+	}
+
+}
