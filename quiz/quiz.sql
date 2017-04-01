ae017055ab04b4ce58883cd8d637fed293cb96fb
vsrinivasan <Vijayan_Srinivasan@intuit.com>
2017-04-01 12:11:09 +0530

    Sample Web Project

---
+++ quiz/quiz.sql
@@ -0,0 +1,152 @@
+-- MySQL dump 10.13  Distrib 5.6.24, for osx10.8 (x86_64)
+--
+-- Host: 127.0.0.1    Database: quiz
+-- ------------------------------------------------------
+-- Server version	5.6.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `QUIZCONTACT`
+--
+
+DROP TABLE IF EXISTS `QUIZCONTACT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `QUIZCONTACT` (
+  `NAME` varchar(4000) DEFAULT NULL,
+  `EMAIL` varchar(4000) DEFAULT NULL,
+  `PHONE` varchar(4000) DEFAULT NULL,
+  `MESSAGE` varchar(4000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `QUIZCONTACT`
+--
+
+LOCK TABLES `QUIZCONTACT` WRITE;
+/*!40000 ALTER TABLE `QUIZCONTACT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `QUIZCONTACT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `QUIZINFO`
+--
+
+DROP TABLE IF EXISTS `QUIZINFO`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `QUIZINFO` (
+  `SUBJECT` varchar(4000) DEFAULT NULL,
+  `QUIZNAME` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`QUIZNAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `QUIZINFO`
+--
+
+LOCK TABLES `QUIZINFO` WRITE;
+/*!40000 ALTER TABLE `QUIZINFO` DISABLE KEYS */;
+/*!40000 ALTER TABLE `QUIZINFO` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `QUIZQ`
+--
+
+DROP TABLE IF EXISTS `QUIZQ`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `QUIZQ` (
+  `NAME` varchar(4000) DEFAULT NULL,
+  `EMAIL` varchar(4000) DEFAULT NULL,
+  `PHONE` varchar(4000) DEFAULT NULL,
+  `QUESTION` varchar(4000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `QUIZQ`
+--
+
+LOCK TABLES `QUIZQ` WRITE;
+/*!40000 ALTER TABLE `QUIZQ` DISABLE KEYS */;
+/*!40000 ALTER TABLE `QUIZQ` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `QUIZQUES`
+--
+
+DROP TABLE IF EXISTS `QUIZQUES`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `QUIZQUES` (
+  `QUESTION` varchar(4000) DEFAULT NULL,
+  `OPTION1` varchar(4000) DEFAULT NULL,
+  `OPTION2` varchar(4000) DEFAULT NULL,
+  `OPTION3` varchar(4000) DEFAULT NULL,
+  `OPTION4` varchar(4000) DEFAULT NULL,
+  `ANSWER` varchar(4000) DEFAULT NULL,
+  `QUIZNAME` varchar(4000) DEFAULT NULL,
+  `QID` int(11) NOT NULL AUTO_INCREMENT,
+  `DESCRIPTION` varchar(4000) DEFAULT NULL,
+  PRIMARY KEY (`QID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `QUIZQUES`
+--
+
+LOCK TABLES `QUIZQUES` WRITE;
+/*!40000 ALTER TABLE `QUIZQUES` DISABLE KEYS */;
+/*!40000 ALTER TABLE `QUIZQUES` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `QUIZREGISTER`
+--
+
+DROP TABLE IF EXISTS `QUIZREGISTER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `QUIZREGISTER` (
+  `USERNAME` varchar(4000) DEFAULT NULL,
+  `USERPASS` varchar(4000) DEFAULT NULL,
+  `CATEGORY` varchar(4000) DEFAULT NULL,
+  `EMAIL` varchar(4000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `QUIZREGISTER`
+--
+
+LOCK TABLES `QUIZREGISTER` WRITE;
+/*!40000 ALTER TABLE `QUIZREGISTER` DISABLE KEYS */;
+/*!40000 ALTER TABLE `QUIZREGISTER` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-01 11:48:24
